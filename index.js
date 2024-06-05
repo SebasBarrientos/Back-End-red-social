da3a1437319123bade5e2760fc6957548deebaca
Sebastian <sebas33696@gmail.com>
2024-06-05 13:29:50 +0200

    Agregado para visualizacion de fotos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { dbConnection } = require("./config/config")
 const app = express()
+const path = require ('path')
 require("dotenv").config();
 const cors = require("cors")
 app.use(cors())
